999a1f6058829542827ec56a77efb9ac28ca9e3e
Ian Conery <ianconery@gmail.com>
2015-10-30 19:20:00 -0700

    added palindromeChecker

---
+++ Problems/palindromeProblem.js
@@ -0,0 +1,8 @@
+//Given a string write a method that returns true if the string is a palindrome and false if it is not
+//Example palindromeChecker('racecar') output: true palindromeChecker('hello') output: false
+//A palindrome is a word that is spelled the same forwards as it is backwards
+
+
+var palindromeChecker = function(){
+  //Implement your version of palindromeChecker here!
+};
